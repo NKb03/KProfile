71f6404392426d9542677c3bdc6c2a7fb6d694c2
NKb03 <niko.knop003@gmail.com>
2018-09-09 10:37:22 +0200

    Renamed Units.kt to Resources.kt
    Added function ofNanos to companion of Time

---
+++ src/main/kotlin/nikok/kprofile/api/Resources.kt
@@ -0,0 +1,46 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package nikok.kprofile.api
+
+import java.io.Serializable
+
+interface Resource: Serializable
+
+@Suppress("EnumEntryName")
+enum class MemoryUnit: Serializable { byte, kilobyte, megabyte, gigabyte, terrabyte }
+@Suppress("EnumEntryName")
+enum class MemoryConsumptionKind: Serializable { RAM, disc }
+data class Memory(
+    var kind: MemoryConsumptionKind,
+    var value: Long,
+    var unit: MemoryUnit
+): Resource {
+    override fun toString(): String {
+        return "consumed $value $unit of $kind"
+    }
+}
+
+@Suppress("EnumEntryName")
+enum class TimeUnit: Serializable {
+    nanoseconds, microseconds, milliseconds, seconds, minutes;
+}
+class Time private constructor(private val value: Long, private val unit: TimeUnit): Resource {
+    override fun toString(): String {
+        return "took $value $unit"
+    }
+
+    companion object {
+        fun ofNanos(nanos: Long): Time {
+            return when {
+                nanos > 60_000_000_000 -> Time(nanos / 60_000_000_000, TimeUnit.minutes)
+                nanos > 1_000_000_000 -> Time(nanos / 1_000_000_000, TimeUnit.seconds)
+                nanos > 1_000_000_000 -> Time(nanos / 1_000_000, TimeUnit.milliseconds)
+                nanos > 1_000 -> Time(nanos / 1_000, TimeUnit.microseconds)
+                nanos > 0 -> Time(nanos, TimeUnit.nanoseconds)
+                else -> throw IllegalArgumentException("nanos must be non-negative")
+            }
+        }
+    }
+}
