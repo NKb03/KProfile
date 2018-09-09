8e0466352939fcde581e91297c20bd6af2b1d769
NKb03 <niko.knop003@gmail.com>
2018-09-09 10:34:12 +0200

    Cleaned up API

--- src/main/kotlin/nikok/kprofile/api/API.kt
+++ src/main/kotlin/nikok/kprofile/api/API.kt
@@ -4,36 +4,48 @@
 
 package nikok.kprofile.api
 import nikok.kprofile.impl.ProfileBodyImpl
-import java.io.PrintStream
 import java.nio.file.Files
 import java.nio.file.Files.createFile
 import java.nio.file.Files.exists
+import java.nio.file.Path
 import java.nio.file.Paths
 
-fun profile(topic: String, vararg tags: Tag, body: ProfileBody.() -> Unit) {
-    val pb = ProfileBodyImpl(topic, tags.asList())
+fun profile(topic: String, tags: List<Tag>, body: ProfileBody.() -> Unit) {
+    val pb = ProfileBodyImpl(topic, tags)
     pb.body()
     pb.write()
 }
 
-fun view(topic: String, vararg tags: Tag, out: PrintStream = System.out) {
-    plainText.view(topic, *tags, out = out)
-}
-
-fun view(topic: String, vararg tags: Tag, fileName: String) {
-    markdown().view(topic, tags = *tags, fileName = fileName)
-}
-
-fun diff(topic: String, vararg tagss: List<Tag>, out: PrintStream = System.out) {
-    plainText.diff(topic, *tagss, out = out)
-}
-
-fun ResultFormat.view(topic: String, vararg tags: Tag, fileName: String) {
+fun ResultFormat.view(topic: String, tags: List<Tag>, fileName: String) {
     val path = Paths.get(fileName)
     if (!exists(path)) createFile(path)
     val out = Files.newOutputStream(path).bufferedWriter()
-    view(topic, *tags, out = out)
+    view(topic, tags, out)
     out.close()
+}
+
+fun ResultFormat.view(topic: String, tags: List<Tag>) {
+    view(topic, tags, System.out)
+    System.out.flush()
+}
+
+fun view(topic: String, tags: List<Tag>) {
+    plainText.view(topic, tags)
+}
+
+
+fun ResultFormat.diff(topic: String, path: Path, vararg tags: List<Tag>) {
+    val out = Files.newBufferedWriter(path)
+    diff(topic, out, *tags)
+    out.close()
+}
+
+fun ResultFormat.diff(topic: String, vararg tags: List<Tag>) {
+    diff(topic, System.out, *tags)
+}
+
+fun diff(topic: String, vararg tags: List<Tag>) {
+    plainText.diff(topic, *tags)
 }
 
 
